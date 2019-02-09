1a7735906427fb40bcefd526007fb4d300deef88
jonlawrence83@gmail.com <jonlawrence83@gmail.com>
2019-02-09 01:01:32 -0700

    more styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,19 +5,19 @@ import './footer.css';
 class Footer extends Component {
      render() {
           return (
-               <Jumbotron className="social text-center">
-               <h1>Connect With Us</h1>
-               <h3>Keep up to date on what we're working on next</h3>
-                    <div class="col-12">
-                         <a href="#"><i class="fab fa-facebook"></i></a>
-                         <a href="#"><i class="fab fa-instagram"></i></a>
-                         <a href="#"><i class="fab fa-twitter-square"></i></a>
-                         <a href="#"><i class="fab fa-youtube-square"></i></a>
-                         <a href="#"><i class="fab fa-github-square"></i></a>
+               <Jumbotron className="social text-center" id="connect">
+                    <h1>Connect With Us</h1>
+                    <h3>Keep up to date on what we're working on next</h3>
+                    <div class="col-sm-12">
+                         <a href="/"><i class="fab fa-facebook"></i></a>
+                         <a href="/"><i class="fab fa-instagram"></i></a>
+                         <a href="/"><i class="fab fa-twitter-square"></i></a>
+                         <a href="/"><i class="fab fa-youtube-square"></i></a>
+                         <a href="/"><i class="fab fa-github-square"></i></a>
                     </div>
                     <br />
                     <div>
-                        <p><i class="far fa-copyright"></i>Roadie 2019</p>
+                         <p><i class="far fa-copyright"></i>Roadie 2019</p>
                     </div>
                </Jumbotron>
           )
--- client/src/components/layout/Search.js
+++ client/src/components/layout/Search.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import LocationSearchInput from './Autocomplete';
 import Footer from './../Footer/Footer';
+import { Container } from 'react-bootstrap';
+import './search.css';
 
 class Search extends Component {
 
@@ -26,28 +28,25 @@ class Search extends Component {
   }
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <form className="col s12 align-center" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label for="artistSearch">Artist</label>
-              <input type="text" className="form-control" id="artistSearch" placeholder="Enter artist" value={this.state.artist} onChange={this.handleArtistChange}></input>
-            </div>
-            <div className="form-group">
-              <label for="citySearch">City</label>
-              <LocationSearchInput latLong={(latLng) => this.handleCityChange(latLng)} />
-            </div>
-            <button type="submit" className="btn btn-outline-dark waves-effect waves-light hoverable">Search</button>
-          </form>
+      <Container className="search-box">
+        <div>
+          <div className="row">
+            <form className="col s12 align-center" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <h2><label for="artistSearch">Artist</label></h2>
+                <input type="text" className="form-control" id="artistSearch" placeholder="Enter artist" value={this.state.artist} onChange={this.handleArtistChange}></input>
+              </div>
+              <div className="form-group">
+                <h2><label for="citySearch">City</label></h2>
+                <LocationSearchInput latLong={(latLng) => this.handleCityChange(latLng)} />
+              </div>
+              <button type="submit" className="btn btn-outline-dark">Search</button>
+            </form>
+          </div>
         </div>
         <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
         <Footer />
-      </div>
+      </Container>
     )
   }
 }
